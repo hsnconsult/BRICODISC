81c54db2ae749e689d2d87e1954640129f6e9ee4
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2021-01-14 00:58:21 +0100

    Update dashbord.py

--- hsn_dashbord/models/dashbord.py
+++ hsn_dashbord/models/dashbord.py
@@ -8,7 +8,6 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError, ValidationError
 from odoo import api, fields, models, _
 from dateutil.relativedelta import relativedelta
-from odoo.tools.monthdelta import MonthDelta
     
 
 
