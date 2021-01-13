3a0296fb8d6b21dabbbbc577cf1cc37d36cb0d91
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2021-01-14 00:55:07 +0100

    Add files via upload

---
+++ hsn_dashbord/models/dashbord.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import time
+import math
+import datetime
+from odoo.osv import expression
+from odoo.tools.float_utils import float_round as round
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.exceptions import UserError, ValidationError
+from odoo import api, fields, models, _
+from dateutil.relativedelta import relativedelta
+from odoo.tools.monthdelta import MonthDelta
+    
+
+
+class DashFonction(models.Model):
+    _name = "dashbord.fonction"
+    _description = "Fonction"
+    
+    
+    def asolder(self):
+        dashprod = self.env['dashbord.templatestock']
+        prods = self.env['product.template'].search([('qty_available','>',30)])
+        vendus = self.env['sale.report'].read_group([('date','>','2020-12-11'),('date','<=','2021-01-13')],fields=['product_uom_qty'], groupby=['product_id'])
+        ventes = dict([(data['product_id'][0], data['product_uom_qty']) for data in vendus if data['product_uom_qty']>20])
+        prodvendus = list(ventes.keys())
+        for prod in prods:
+            if prod.qty_available*prod.standard_price > 600000:
+               prodid = self.env['product.product'].search([('product_tmpl_id','=',prod.id)])[0].id
+               if prodid in prodvendus:
+                  vals = {
+                    'name':prod.name,
+                    'default_code':prod.default_code,
+                    'lst_price':prod.lst_price,
+                    'qty_available':prod.qty_available,
+                    'uom_id':prod.uom_id.name,
+                    'dashstate':'sol1'
+                    }
+                  dashprod.create(vals)
+        
+    name = fields.Char('Nom')
+    
+class ProductDash(models.Model):
+    _name = "dashbord.templatestock"
+    _description = "Produit"
+    
+    name = fields.Char('Nom produit')
+    default_code = fields.Char('Référence')
+    barcode = fields.Char('Code barre')
+    lst_price = fields.Float('Prix')
+    qty_available = fields.Float('Quantité')
+    uom_id = fields.Char('Udm')
+    dashstate = fields.Selection([('sol1','SOL 1 PROMO'),('sol2','SOL 2 PROMO'),('sol3','SOL 3 PROMO'),('afaire','A FAIRE'),('encours','EN COURS'),('termine','TERMINE')], string='Etat DASH', size=64, default='sol1' ,track_visibility='onchange', required=True)
+    
